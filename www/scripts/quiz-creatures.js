44911cbf7998048d61d232c9a5db01c0abd5747d
Mark Diehr <mdiehr@gmail.com>
2017-09-07 08:36:04 -0400

    Create creature quiz

---
+++ www/scripts/quiz-creatures.js
@@ -0,0 +1,101 @@
+// Quiz result options in a separate object for flexibility
+var HUMAN_RESULT = {
+    title: 'Human',
+    desc: 
+        '<img width="223px" height="310px" src="https://goodgamery.com/api/mtg/image?q=t:human%20t:creature&sort=random&limit=1&p=_nocache"/><p>'
+        +"Congratulations, you are HUMAN! While many Magic creature types are real animals, many are not. However, bears, spiders, etc. cannot use the internet and therefore can't take this quiz. If you had your dog answer for you, there's nothing in the rules against that, but keep in mind that your dog is also now human as well."
+        +'</p>'
+};
+var resultOptions = [HUMAN_RESULT, HUMAN_RESULT, HUMAN_RESULT, HUMAN_RESULT, HUMAN_RESULT];
+    
+// global variables
+var quizSteps = $('#quizzie .quiz-step'),
+    totalScore = 0;
+
+// for each step in the quiz, add the selected answer value to the total score
+// if an answer has already been selected, subtract the previous value and update total score with the new selected answer value
+// toggle a visual active state to show which option has been selected
+quizSteps.each(function () {
+    var currentStep = $(this),
+        ansOpts = currentStep.children('.quiz-answer');
+    // for each option per step, add a click listener
+    // apply active class and calculate the total score
+    ansOpts.each(function () {
+        var eachOpt = $(this);
+        eachOpt[0].addEventListener('click', check, false);
+        function check() {
+            var $this = $(this),
+                value = $this.attr('data-quizIndex'),
+                answerScore = parseInt(value);
+            // check to see if an answer was previously selected
+            if (currentStep.children('.active').length > 0) {
+                var wasActive = currentStep.children('.active'),
+                    oldScoreValue = wasActive.attr('data-quizIndex'),
+                    oldScore = parseInt(oldScoreValue);
+                // handle visual active state
+                currentStep.children('.active').removeClass('active');
+                $this.addClass('active');
+                // handle the score calculation
+                totalScore -= oldScoreValue;
+                totalScore += answerScore;
+                calcResults(totalScore);
+            } else {
+                // handle visual active state
+                $this.addClass('active');
+                // handle score calculation
+                totalScore += answerScore;
+                calcResults(totalScore);
+                // handle current step
+                updateStep(currentStep);
+            }
+        }
+    });
+});
+
+// show current step/hide other steps
+function updateStep(currentStep) {
+    if(currentStep.hasClass('current')){
+       currentStep.removeClass('current');
+       currentStep.next().addClass('current');
+    }
+}
+
+// display scoring results
+function calcResults(totalScore) {
+    // only update the results div if all questions have been answered
+    if (quizSteps.find('.active').length == quizSteps.length){
+        var resultsTitle = $('#results-title'),
+            resultsDesc = $('#results-desc');
+        
+        // calc lowest possible score
+        var lowestScoreArray = $('#quizzie .low-value').map(function() {
+            return $(this).attr('data-quizIndex');
+        });
+        var minScore = 0;
+        for (var i = 0; i < lowestScoreArray.length; i++) {
+            minScore += lowestScoreArray[i] << 0;
+        }
+        // calculate highest possible score
+        var highestScoreArray = $('#quizzie .high-value').map(function() {
+            return $(this).attr('data-quizIndex');
+        });
+        var maxScore = 0;
+        for (var i = 0; i < highestScoreArray.length; i++) {
+            maxScore += highestScoreArray[i] << 0;
+        }
+        // calc range, number of possible results, and intervals between results
+        var range = maxScore - minScore,
+            numResults = resultOptions.length,
+            interval = range / (numResults - 1),
+            increment = '',
+            n = 0; //increment index
+
+        // populate results
+        var titleText = "<h1>" + resultOptions[n].title + "</h1>";
+        var descText = "<p class='desc'>" + resultOptions[n].desc + "</p>";
+        console.log("Setting result to " + titleText + ": " + descText);
+        resultsTitle.replaceWith(titleText);
+        resultsDesc.replaceWith(descText);
+        return;
+    }
+}
