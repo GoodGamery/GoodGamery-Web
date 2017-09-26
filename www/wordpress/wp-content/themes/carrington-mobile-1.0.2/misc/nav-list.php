3d18cbfdfdc3e54222eb361c36c1e70d0c1155a2
Mark Diehr <mdiehr@gmail.com>
2017-09-25 20:59:03 -0400

    Remove unused themes

--- www/wordpress/wp-content/themes/carrington-mobile-1.0.2/misc/nav-list.php
+++
@@ -1,5 +0,0 @@
-<span>&laquo;</span>
-<span class="next"><?php previous_posts_link('Previous'); ?></span>
-<span>|</span>
-<span class="prev"><?php next_posts_link('Next'); ?></span>
-<span>&raquo;</span>
