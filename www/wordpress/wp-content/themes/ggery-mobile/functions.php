58d614bc7ff5156d8ab80146676e265df9ba2a04
Mark Diehr <mdiehr@gmail.com>
2017-09-25 20:54:08 -0400

    Fix frontpage PMO article display on mobile

--- www/wordpress/wp-content/themes/ggery-mobile/functions.php
+++ www/wordpress/wp-content/themes/ggery-mobile/functions.php
@@ -1,10 +1,10 @@
 <?php
-if ( function_exists('register_sidebar') )
+
+if ( function_exists('register_sidebar') ) {
     register_sidebar();
-?>
-<?php
-function gg_get_custom_fields()
-{
+}
+
+function gg_get_custom_fields() {
     // Magical hackery to display custom posts
     $custom_fields = array('pmo' => '', 'icon' => '', 'author' => '', 'thread' => '');
     $custom_field_keys = get_post_custom_keys();
@@ -26,75 +26,87 @@ function gg_get_custom_fields()
     }
     return $custom_fields;
 }
-?>
-<?php
-function gg_pmo_post()
-{
+
+function gg_pmo_post($isSingle = false) {
     $custom_fields = gg_get_custom_fields();
     $pmo_addr = "https://goodgamery.com/pmo/c" . $custom_fields['pmo'] . ".htm";
-?>
-    <!-- PMO Post -->
-    <table class="post_block">
-        <tr>
-            <td>
-                <a href="<?php echo $pmo_addr; ?>"><img class="icon" src="https://goodgamery.com/icons/pmoguy_icon.jpg"></a>
-            </td>
-            <td>
-                <h2>
+    $pmo_image = "https://goodgamery.com/pmo/c".$custom_fields['pmo'].".png";
+
+
+    if ($isSingle) {
+    ?>
+        <!-- PMO Single PagePost -->
+        <table class="post_block">
+            <tr>
+                <td>
                     <a href="<?php echo $pmo_addr; ?>" rel="bookmark" title="Permanent Link to <?php echo the_title(); ?>">
-                        <?php echo the_title(); ?>
+                        <img src="<?php echo $pmo_image; ?>" style="width:100%;"/>
                     </a>
-                </h2>
-                <span class="author">by anonymous</span>
-                <small><?php the_time('Y-m-d') ?></small>
-                <span class="postmetadata"><?php the_category(', ') ?><?php the_tags(' | ', ', ', ''); ?><?php edit_post_link('Edit', ' | ', ''); ?> </span>
-            </td>
-        </tr>
-    </table>
-<?php 
+                </td>
+            </tr>
+        </table>
+    <?php
+    } else {
+    ?>
+        <!-- PMO Post -->
+        <table class="post_block">
+            <tr>
+                <td>
+                    <a href="<?php echo $pmo_addr; ?>"><img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/pmoguy_icon.jpg"></a>
+                </td>
+                <td>
+                    <h2>
+                        <a href="<?php echo $pmo_addr; ?>" rel="bookmark" title="Permanent Link to <?php echo the_title(); ?>">
+                            <?php echo the_title(); ?>
+                        </a>
+                    </h2>
+                    <span class="author">by anonymous</span>
+                    <small><?php the_time('Y-m-d') ?></small>
+                    <span class="postmetadata"><?php the_category(', ') ?><?php the_tags(' | ', ', ', ''); ?><?php edit_post_link('Edit', ' | ', ''); ?> </span>
+                </td>
+            </tr>
+        </table>
+    <?php 
+    }
 }
-?>
-<?php 
-function gg_post_enhanced()
-{
+
+function gg_post_enhanced() {
     $custom_fields = gg_get_custom_fields();
-?>
-    <!-- Custom Enhanced Post - uses the_excerpt, and the custom fields icon and author -->
-    <table class="post_block">
-        <tr>
-            <td>
-                <a href="<?php the_permalink() ?>">
-                    <?php if( $custom_fields['card'] != '') : ?>
-                        <div class="blank_icon"><div class="icon_card"><img alt="Card Icon" src="<?php echo $custom_fields['card']; ?>" /></div></div>
-                    <?php elseif( $custom_fields['icon'] == '') : ?>
-                        <img class="icon" alt="Missing Article Icon" src="https://goodgamery.com/icons/error_icon.jpg" />
-                    <?php else : ?>
-                        <img class="icon" alt="Article Icon" src="<?php echo $custom_fields['icon']; ?>" />
-                    <?php endif; ?>
-                </a>
-            </td>
-            <td>
-                <h2>
-					<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title() ?>"><?php the_title() ?></a>
-				</h2>
-                <span class="author"><?php echo $custom_fields['author']; ?></span>
-                <small><?php the_time('Y-m-d') ?></small>
-                <span class="postmetadata"><?php the_category(', ') ?><?php the_tags(' | ', ', ', ''); ?><?php edit_post_link('Edit', ' | ', ''); ?> </span>
-            </td>
-        </tr>
-    </table>
-<?php 
+    ?>
+        <!-- Custom Enhanced Post - uses the_excerpt, and the custom fields icon and author -->
+        <table class="post_block">
+            <tr>
+                <td>
+                    <a href="<?php the_permalink() ?>">
+                        <?php if( $custom_fields['card'] != '') : ?>
+                            <div class="blank_icon"><div class="icon_card"><img alt="Card Icon" src="<?php echo $custom_fields['card']; ?>" /></div></div>
+                        <?php elseif( $custom_fields['icon'] == '') : ?>
+                            <img class="icon" alt="Missing Article Icon" src="https://goodgamery.com/icons/error_icon.jpg" />
+                        <?php else : ?>
+                            <img class="icon" alt="Article Icon" src="<?php echo $custom_fields['icon']; ?>" />
+                        <?php endif; ?>
+                    </a>
+                </td>
+                <td>
+                    <h2>
+    					<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title() ?>"><?php the_title() ?></a>
+    				</h2>
+                    <span class="author"><?php echo $custom_fields['author']; ?></span>
+                    <small><?php the_time('Y-m-d') ?></small>
+                    <span class="postmetadata"><?php the_category(', ') ?><?php the_tags(' | ', ', ', ''); ?><?php edit_post_link('Edit', ' | ', ''); ?> </span>
+                </td>
+            </tr>
+        </table>
+    <?php 
 }
-?>
-<?php 
-function gg_thread_link()
-{
+
+function gg_thread_link() {
     $custom_fields = gg_get_custom_fields();
     if( $custom_fields['thread'] != '' ) :
-?>
-    <br/><br/>
-    (<a href="<?php echo $custom_fields['thread'] ?>">Discuss this item in the forums!</a>)
-<?php 
+    ?>
+        <br/><br/>
+        (<a href="<?php echo $custom_fields['thread'] ?>">Discuss this item in the forums!</a>)
+    <?php 
     endif;
 }
 ?>
